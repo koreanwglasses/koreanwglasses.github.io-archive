ad1d6e61ee085a360d88b59c06d3ec4e27fc953b
koreanwglasses <koreanwglasses@gmail.com>
2019-07-22 11:14:52 -0400

    Fixed autoscroll

--- src/shell/shell.tsx
+++ src/shell/shell.tsx
@@ -93,8 +93,9 @@ export class Shell {
     this.lineBufferEditor.show();
     this.terminal.render();
 
-    if (this.willKeepCommandInView && this.promptRefs.length > 2) {
+    if (this.willKeepCommandInView && this.promptRefs[this.promptRefs.length - 2]) {
       this.promptRefs[this.promptRefs.length - 2].scrollIntoView();
+      this.willKeepCommandInView = false;
     }
   }
 
