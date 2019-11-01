d52d5ae5da47a0008029bc9448103e6dfda70a3f
koreanwglasses <koreanwglasses@gmail.com>
2019-11-01 00:02:43 -0400

    Serving React libs while fixing CORS

--- template.prod.js
+++ template.prod.js
@@ -1,7 +1,7 @@
 const template = require('./template')
 const externals = `
-    <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
-    <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+    <script src="/lib/react.production.min.js"></script>
+    <script src="/lib/react-dom.production.min.js"></script>
 `
 const mainScript = '<script src="/js/scripts.js"></script>'
 module.exports = args => template({...args, externals, mainScript});
