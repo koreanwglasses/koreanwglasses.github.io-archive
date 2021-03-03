17950d8877d13cdcb4d6b70e12bdae92833b0c32
koreanwglasses <koreanwglasses@gmail.com>
2021-03-03 14:55:32 -0600

    added footer

---
+++ src/components/hspace.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const HSpace = ({ width="2px" }: Pick<React.CSSProperties, 'width'>) => <span style={{ display: "inline-block", width }}/>
+
+export default HSpace
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
+import Footer from "./footer";
 import Header from "./header";
 import "./layout.css";
 import * as styles from "./layout.module.css";
 
 const Layout = ({ children }: React.PropsWithChildren<{}>) => (
-  <div className={styles.contentContainer}>
+  <div className={styles.pageContainer}>
     <Header />
+    <div className={styles.contentContainer}>
     {children}
+    </div>
+    <Footer/>
   </div>
 );
 
