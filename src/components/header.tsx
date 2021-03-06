83d6f7c61f37366a15223b56c131e76e6b947ad8
koreanwglasses <koreanwglasses@gmail.com>
2021-03-06 14:37:22 -0600

    name goes back home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
 import * as styles from "./header.module.css";
 import HLine from "./hline";
 import * as React from "react";
+import ContainerLink from "./container-link";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1>Fred Choi</h1>
+      <ContainerLink to="/"><h1>Fred Choi</h1></ContainerLink>
       <HLine />
     </div>
   );
